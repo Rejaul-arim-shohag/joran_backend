64b7ce65370bdb2eb0dce8736293de2755a0697e
Rejaul karim <76891535+Rejaul-arim-shohag@users.noreply.github.com>
2023-06-19 14:26:20 +0600

    done

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -20,9 +20,8 @@ exports.allProducts = (req, res) => {
     })
 }
 
-exports.updateProduct = (req, res) => {
-    const product_id = req.headers.product_id
-    productModel.updateOne({ product_id }, req.body, (err, data) => {
+exports.findProductById = (req, res) => {
+    productModel.findOne({_id: req.body.product_id}, (err, data) => {
         if (err) {
             res.status(500).json({ "status": "fail", "error": err })
         } else {
@@ -31,8 +30,28 @@ exports.updateProduct = (req, res) => {
     })
 }
 
+
+exports.updateProduct = (req, res) => {
+    const product_id = req.body.product_id
+    const reqBody={
+        product_name: req.body.product_name,
+        price: req.body.price,
+        category_name: req.body.category_name,
+        image: req.bodyimageUrl
+    }
+    productModel.updateOne({ _id :product_id }, reqBody, (err, data) => {
+        if (err) {
+            res.status(500).json({ "status": "fail", "error": err })
+        } else {
+            console.log(res.data)
+            res.status(200).json({ "status": "success", "data": data })
+        }
+    })
+}
+
 exports.deleteProduct = async (req, res) => {
-    const { productIds } = req.body;
+    const  productIds  = req.body;
+    console.log("productIds", req.body)
     try {
         for (let i = 0; i < productIds.length; i++) {
             const productId = productIds[i];
@@ -41,6 +60,7 @@ exports.deleteProduct = async (req, res) => {
         }
         res.status(200).json({ message: 'Products deleted successfully' });
     } catch (error) {
+        console.log("err", error)
         res.status(500).json({ message: 'Error deleting products', error });
     }
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -9,11 +9,12 @@ const userVerifyMiddleware = require('../middleware/userVerifyMiddleware');
 //user endpoints
 router.post("/user_signup", userController.userSignup);
 router.post("/user_login", userController.UserLogin);
-router.post("/user_logout", userController.UserLogout);
+router.post("/user_logout",userVerifyMiddleware, userController.UserLogout);
 
 //product endpoints
 router.post("/productCreate",userVerifyMiddleware, productController.productInsert);
 router.get("/allProducts",userVerifyMiddleware, productController.allProducts);
+router.post(`/findProductById`,userVerifyMiddleware, productController.findProductById);
 router.post("/updateProduct",userVerifyMiddleware, productController.updateProduct);
 router.post("/deleteProduct",userVerifyMiddleware, productController.deleteProduct);
 
